876ce04e9e457f973e4501bd958ddf0c9958e045
stephenChinag <stephenchinag@gmail.com>
2025-04-02 12:32:41 +0100

    fix

--- obj/Debug/net8.0/SingnalRDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/SingnalRDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingnalRDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45f5ae3c417651ff9266deaacf036f3ef49b94ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingnalRDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingnalRDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
